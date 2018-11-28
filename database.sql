d2f82d7de216f906b56363004747339422ef28a5
Emil Toivainen <37114731+Em1lT@users.noreply.github.com>
2018-11-28 13:57:14 +0200

    update to the db commands

--- database.sql
+++ database.sql
@@ -4,3 +4,16 @@ Create TABLE users1 (
     password TEXT,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE uploaded (
+    id INT AUTO_INCREMENT,
+    product TEXT,
+    price TEXT,
+    year TEXT,
+    stars TEXT,
+    model TEXT,
+    thumbnail TEXT,
+    image TEXT,
+    original TEXT,
+    PRIMARY KEY(id)
+);
